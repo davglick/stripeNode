2f7106d7ca91b570be03e856a8b927f4584a8680
Dav Glick <dav.glick18@mail.com>
2016-11-29 11:21:26 +1100

    initialized repo

---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+
+	requireAuthnetication: function (req, res, next) {
+		console.log('private route hit!');
+		next();
+	}
+};
+
+app.use(middleware.requireAuthnetication)
+
+app.get('/', function(req, res){
+	res.send("hello world")
+
+});
+
+console.log("running on server host " + PORT )
+app.listen(PORT);
+
+
+app.get('/customer', function (res, res){
+    
+    var customerId = 'user'
+
+});
+
+console.log(__dirname);
+
+// Using Express (http://expressjs.com/)
+app.get('/customer', function(request, response) {
+  var customerId = '...'; // Load the Stripe Customer ID for your logged in user
+  stripe.customers.retrieve(customerId, function(err, customer) {
+    if (err) {
+      response.status(402).send('Error retrieving customer.');
+    } else {
+      response.json(customer);
+    }
+
+  })
+
+});
+
