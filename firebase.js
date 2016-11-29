5101a81eb3cab32189257a7f3cf3a2bdc010e2f7
Dav Glick <dav.glick18@mail.com>
2016-11-30 10:19:06 +1100

    g

--- firebase.js
+++ firebase.js
@@ -1,3 +1,4 @@
+/*
 var firebase = require('firebase');
 var express = require('express');
 var app = express();
@@ -79,3 +80,71 @@ app.get('/customer', function(req, res) {
   res.send(customerId);
 
 });
+
+
+*/
+
+var firebase = require('firebase');
+var express = require('express');
+var app = express();
+var admin = require("firebase-admin");
+var PORT = process.env.PORT || 3000;
+//var bodyParser = require("body-parser");
+var hbs = require("hbs");
+var stripe = require("stripe")("sk_test_ndoQxTKblloCG2EDELfg3kJT");
+
+
+// Initialize Firebase
+
+var config = {
+    apiKey: "AIzaSyCnt6EppVeps5n9h6sVEFX-UJAYOQtHNRo",
+    authDomain: "appearprofile.firebaseapp.com",
+    databaseURL: "https://appearprofile.firebaseio.com",
+    storageBucket: "appearprofile.appspot.com",
+    messagingSenderId: "522018329506"
+  };
+  firebase.initializeApp(config);
+
+
+var userId = "asfjk3234f3454gerpt4"
+var auth = firebase.auth();
+var ref = firebase.database().ref();
+var paymentRef = ref.child('CCard/' + userId );
+var paymentRef = paymentRef.push();
+var user = firebase.auth().currentUser;
+console.log(paymentRef.key);
+console.log(PORT);
+
+
+// check for auth state chagnes 
+
+initApp = function() {
+        firebase.auth().onAuthStateChanged(function(user) {
+          if (user) {
+
+
+          } else {
+
+          	paymentRef.set({
+	token: '1234433asvcrwe2wrsfdv',
+	card: 'Visa',
+	default: false
+
+          });
+
+          	console.log('nul')
+
+
+     }
+
+ })
+};
+
+app.get('/', function(req, res){
+
+	res.send('Doing!');
+});
+
+initApp()
+app.listen(3000);
+
