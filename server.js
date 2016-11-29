bd779984d7a604ee0fb8583aead4143196a048ad
Dav Glick <dav.glick18@mail.com>
2016-11-29 11:57:18 +1100

    added stripe and new modules

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var stripe = require("stripe")(
+  "sk_test_ndoQxTKblloCG2EDELfg3kJT"
+ var userId = '2343423gjvf3q4234r2f'
+);
 
 
 app.get('/', function(req, res){
@@ -18,12 +22,10 @@ app.get('/customer', function (res, res){
 
 });
 
-console.log(__dirname);
-console.log("hello")
 
 // Using Express (http://expressjs.com/)
 app.get('/customer', function(request, response) {
-  var customerId = '...'; // Load the Stripe Customer ID for your logged in user
+  var customerId = userId; // Load the Stripe Customer ID for your logged in user
   stripe.customers.retrieve(customerId, function(err, customer) {
     if (err) {
       response.status(402).send('Error retrieving customer.');
